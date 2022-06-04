04c3cde41f8c96fd3beedcc6b8a02ddfcf16ec43
Elle Meredith <ellemeredith@gmail.com>
2022-06-04 15:16:12 +1000

    Log user in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   mount GoodJob::Engine => "good_job"
 
+  get "/login", to: "sessions#new", as: :login
+  delete "/logout", to: "sessions#destroy", as: :logout
+
   resources :confirmations, only: [:new]
   resource :dashboard, only: [:show]
   resources :posts, only: [:show]
+  resources :sessions, only: [:new, :create]
 
   root "dashboard#show"
 end
