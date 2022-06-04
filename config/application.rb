a112cdf876fe12cc06fae750ba4eee1c4804f2ee
Elle Meredith <ellemeredith@gmail.com>
2022-06-04 15:18:20 +1000

    Set up Responders gem

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ Bundler.require(*Rails.groups)
 
 module TddWorkshop
   class Application < Rails::Application
+    # Use the responders controller from the responders gem
+    config.app_generators.scaffold_controller :responders_controller
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
