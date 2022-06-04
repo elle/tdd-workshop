79ac4d2b2659830adeecc2470c4a2c43de959306
Elle Meredith <ellemeredith@gmail.com>
2022-06-04 17:04:11 +1000

    Prep for Dashboard

---
+++ spec/models/dashboard_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Dashboard do
+  describe "#todays_posts" do
+    #it "returns posts created today"
+  end
+
+  describe "#posts" do
+    #it "returns posts visible to the current user"
+  end
+end
+
