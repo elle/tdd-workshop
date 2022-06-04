bb3c99a59a0814f34b99d36da07a1961bd79a099
Elle Meredith <ellemeredith@gmail.com>
2022-06-04 11:30:37 +1000

    Add Post's validations, associations, and scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,20 @@
 class Post < ApplicationRecord
-  belongs_to :person
-
+  belongs_to :person, optional: false
   has_many :comments
+
+  validates :title, presence: true
+
+  def self.published
+    where(published_on: ..Time.current)
+  end
+
+  def self.today
+    start_of_day = Date.current.beginning_of_day
+    end_of_day = Date.current.end_of_day
+    where(published_on: start_of_day..end_of_day)
+  end
+
+  def self.visible(user)
+    where(person: user).or(published)
+  end
 end
