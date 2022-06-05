25d7919550bee7cb9300ce26858cde245bb204f1
Elle Meredith <ellemeredith@gmail.com>
2022-06-06 06:51:15 +1000

    Define default url option for action mailer in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: "example.com" }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
