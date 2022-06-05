e7487d4954f05b0d7a871a7388ccc184936a2a26
Elle Meredith <ellemeredith@gmail.com>
2022-06-05 11:09:59 +1000

    Add status enum to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :person
 
   has_many :comments
+
+  enum status: {
+    draft: 0,
+    finished: 1,
+    reviewed: 2,
+    accepted: 3,
+    published: 4,
+    promoted: 5,
+    rejected: 6,
+    archived: 7,
+  }
 end
