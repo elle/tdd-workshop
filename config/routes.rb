075b6ef0aaedf91071f21a96ae42571bd9d468b9
Elle Meredith <ellemeredith@gmail.com>
2022-06-06 07:09:47 +1000

    Add confirmations#new controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount GoodJob::Engine => "good_job"
+
+  resources :confirmations, only: [:new]
   resource :dashboard, only: [:show]
   resources :posts, only: [:show]
 
