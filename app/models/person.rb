2551829ada5007482d239408651d2e7d95852136
Elle Meredith <ellemeredith@gmail.com>
2022-06-06 11:38:41 +1000

    Add specs for Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,20 @@
 class Person < ApplicationRecord
+  validates :admin, inclusion: { in: [true, false]}
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   after_initialize :set_token
+
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def full_name
+    [first_name, last_name].compact.join(" ").titleize
+  end
 
   private
 
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,26 +2,74 @@ require "rails_helper"
 
 RSpec.describe Person do
   context "validations" do
-    # add validations first_name. email, admin
+    subject { build(:person) }
+
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to allow_value(false).for(:admin) }
+    it { is_expected.to allow_value(true).for(:admin) }
+    it { is_expected.not_to allow_value(nil).for(:admin) }
   end
 
   context "associations" do
-    # has_many posts, dependent
+    it { is_expected.to have_many(:posts).dependent(:destroy) }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
   end
 
   describe "#admin" do
-    # defaults to false, otherwise returns true
+    it "defaults to false" do
+      person = described_class.new
+
+      expect(person).not_to be_admin
+      # expect(person.admin).to eq false
+      # expect(person.admin).to be_falsy
+    end
   end
 
   describe "#full_name" do
-    # when only first name is present -> returns first_name
-    # when both first and last, returns both
-    # returns a titleized full name
+    context "when only first name is present" do
+      it "returns first_name" do
+        person = described_class.new(first_name: "Surya")
+
+        result = person.full_name
+
+        expect(result).to eq "Surya"
+      end
+    end
+
+    context "when both first and last names are present" do
+      it "returns first and last names" do
+        person = described_class.new(first_name: "Surya", last_name: "Setiyaputra")
+
+        result = person.full_name
+
+        expect(result).to eq "Surya Setiyaputra"
+      end
+    end
+
+    it "returns a titleized full name" do
+      person = described_class.new(first_name: "thor", last_name: "odinson")
+
+      result = person.full_name
+
+      expect(result).to eq "Thor Odinson"
+    end
   end
 
   describe "#confirmed?" do
-    # when present -> returns true
-    # when nil -> returns false
+    context "when confirmed_at is present" do
+      it "returns true" do
+        person = described_class.new(confirmed_at: Time.current)
+
+        expect(person).to be_confirmed
+      end
+    end
+
+    context "when confirmed_at is nil" do
+      it "return false" do
+        expect(described_class.new).not_to be_confirmed
+      end
+    end
   end
 
   describe "#set_token" do
