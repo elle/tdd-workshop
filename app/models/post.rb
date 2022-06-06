2f2cc8f1a03c434e323ac6cfdab578ffeb99f6e0
Elle Meredith <ellemeredith@gmail.com>
2022-06-06 11:59:44 +1000

    Start on Post specs

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,9 @@ class Post < ApplicationRecord
   belongs_to :person
 
   has_many :comments
-
-  enum status: {
-    draft: 0,
-    finished: 1,
-    reviewed: 2,
-    accepted: 3,
-    published: 4,
-    promoted: 5,
-    rejected: 6,
-    archived: 7,
-  }
+  
+  def self.published
+    # where("published_on <= ?", Time.current)
+    where(published_on: ..Time.current)
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,37 @@ require "rails_helper"
 RSpec.describe Post do
   include ActiveSupport::Testing::TimeHelpers
 
-  context "validations"
-  context "associations"
+  context "validations" do
+    # validate title, and body, person
+  end
+
+  context "associations" do
+    # belongs to person
+    # has_many comments
+  end
+
+  describe ".published" do
+    context "when published_on is set to today or the past timestamp" do
+      it "returns the past or today's posts" do
+        _draft = create(:post, title: "Draft", published_on: nil)
+        yesterday_post = create(:post, title: "Yesterday", published_on: 1.day.ago)
+        todays_post = create(:post, title: "Today", published_on: Time.current)
+        future_post = create(:post, title: "Tomorrow", published_on: 1.day.from_now)
+
+        result = described_class.published
+
+        expect(result).to eq [yesterday_post, todays_post] # match_array # include
+      end
+    end
+  end
+
+  describe ".today" do
+    # return only posts from today
+  end
+
+  describe ".visible?" do
+    # return all published posts by anyone
+    # if the posts are by me `Post.visible?(user)`
+    #   -> return all my posts as well, regardless whether they are published or not
+  end
 end
