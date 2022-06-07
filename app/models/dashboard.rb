2e86899e7dc3c0e3506a193d2d9e5684f03f398a
Elle Meredith <ellemeredith@gmail.com>
2022-06-07 14:15:36 +1000

    Add specs to Dashboard

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,2 +1,16 @@
 class Dashboard
+  attr_reader :posts, :user
+
+  def initialize(posts:, user:)
+    @posts = posts
+    @user = user
+  end
+
+  def todays_posts
+    posts.today
+  end
+
+  def visible_posts
+    posts.visible(user)
+  end
 end
--- spec/models/dashboard_spec.rb
+++ spec/models/dashboard_spec.rb
@@ -2,11 +2,26 @@ require "rails_helper"
 
 RSpec.describe Dashboard do
   describe "#todays_posts" do
-    #it "returns posts created today"
+    it "returns posts created today" do
+      # should test that when we call `todays_posts` it calls `.today` on Post
+      allow(Post).to receive(:today)
+      dashboard = Dashboard.new(posts: Post.all, user: double)
+
+      dashboard.todays_posts
+
+      expect(Post).to have_received(:today)
+    end
   end
 
-  describe "#posts" do
-    #it "returns posts visible to the current user"
+  describe "#visible_posts" do
+    it "returns posts visible to the current user" do
+      user = double(:user)
+      dashboard = Dashboard.new(posts: Post.all, user: user)
+      allow(Post).to receive(:visible).with(user)
+
+      dashboard.visible_posts
+
+      expect(Post).to have_received(:visible).with(user)
+    end
   end
 end
-
